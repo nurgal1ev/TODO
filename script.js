745d904fa417c576a6033ceee28152d71a1fcef8
indev.js <nurgaliev0644@gmail.com>
2025-05-02 11:49:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const addInput = document.querySelector('#addInput');
+const textInput = document.querySelector('#text-input');
+const taskContainer = document.querySelector('#taskContainer');
+
+
+
+addInput.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const taskInput = textInput.value;
+    const taskAtWork = `
+                    <div class="todo-worked__task">
+                        <h2 class="task-name">${taskInput}</h2>
+                        <div class="buttons">
+                            <button class="input-button" id="buttonDone"><img src="icons/Check-green.svg" alt="" srcset=""></button>
+                            <button class="input-button delete" data-action="delete" id="buttonDelete"><img src="icons/delete.svg" alt="" srcset=""></button>
+                        </div>
+                    </div>`;
+    
+    taskContainer.insertAdjacentHTML('beforeend', taskAtWork)
+
+    textInput.value = '';
+    textInput.focus();
+})
+
+taskContainer.addEventListener('click', function (event) {
+    console.log(event.currentTarget);
+    
+});
