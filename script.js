f5a399c89312a5a390561ad6907031580668bb0f
Eldar N. <n.el34@ya.ru>
2025-05-03 12:53:18 +0300

    function completed task

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addInput = document.querySelector('#addInput');
 const textInput = document.querySelector('#text-input');
 const taskContainer = document.querySelector('#taskContainer');
-
+const taskDoneContainer = document.querySelector('#taskDone')
 
 
 addInput.addEventListener('submit', function (event) {
@@ -9,10 +9,10 @@ addInput.addEventListener('submit', function (event) {
     const taskInput = textInput.value;
     const taskAtWork = `
                     <div class="todo-worked__task">
-                        <h2 class="task-name">${taskInput}</h2>
+                        <h2 class="task-name" id="nameTask">${taskInput}</h2>
                         <div class="buttons">
-                            <button class="input-button" id="buttonDone"><img src="icons/Check-green.svg" alt="" srcset=""></button>
-                            <button class="input-button delete" data-action="delete" id="buttonDelete"><img class="check" src="icons/delete.svg" alt="" srcset=""></button>
+                            <button class="input-button" id="buttonDone" data-action="done"><img class="doneTask" src="icons/Check-green.svg" alt="" srcset=""></button>
+                            <button class="input-button delete" data-action="delete" id="buttonDelete"><img class="deleteTask" src="icons/delete.svg" alt="" srcset=""></button>
                         </div>
                     </div>`;
     
@@ -26,6 +26,22 @@ taskContainer.addEventListener('click', function (event) {
     if (event.target.dataset.action == 'delete') {
         const parentElement = event.target.closest('.todo-worked__task');
         parentElement.remove();
-        
     }
 });
+
+
+taskContainer.addEventListener('click', function (event) {
+    if (event.target.dataset.action == 'done') {
+        const taskElement = event.target.closest('.todo-worked__task');
+        const taskName = taskElement.querySelector('.task-name').textContent;
+        const taskAtDone = `
+            <div class="todo-worked__task">
+                <h2 class="task-name done">${taskName}</h2>
+                <button class="input-button delete" id="buttonDeleteDone"><img src="icons/delete.svg" alt="" srcset=""></button>
+            </div>`;
+    
+        taskDoneContainer.insertAdjacentHTML('beforeend', taskAtDone);
+        taskElement.remove();
+    };
+});
+
